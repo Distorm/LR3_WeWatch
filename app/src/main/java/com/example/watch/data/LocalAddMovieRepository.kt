d18f53d82314b6655369275d3a5a6cbb5eda3915
Distorm_nox <oreshnikov@bk.ru>
2024-05-26 00:03:19 +0700

    Чистая архетектура

--- app/src/main/java/com/example/watch/data/LocalAddMovieRepository.kt
+++ app/src/main/java/com/example/watch/data/LocalAddMovieRepository.kt
@@ -4,7 +4,7 @@ import com.example.watch.domain.AddMovieRepository
 import com.example.watch.models.LocalDataSource
 import com.example.watch.models.Movie
 
-// Data Layer: Здесь находится реализация репозитория, которая взаимодействует с локальным источником данных.
+// Data Layer: Здесь находится реализация репозитория, которая взаимодействует с локальным источником данных 1111.
 class LocalAddMovieRepository(private val dataSource: LocalDataSource) : AddMovieRepository {
     override fun insertMovie(movie: Movie) = dataSource.insert(movie)
 }
